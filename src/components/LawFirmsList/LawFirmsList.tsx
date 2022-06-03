818388af17f1db6e7e6055d18eec90a49efa36f4
maosin77 <73907125+maosin77@users.noreply.github.com>
2022-06-03 09:17:25 +0200

    fix: add empty state for lawFirmList

--- src/components/LawFirmsList/LawFirmsList.tsx
+++ src/components/LawFirmsList/LawFirmsList.tsx
@@ -1,12 +1,21 @@
 import { db } from 'db';
 import { useLiveQuery } from 'dexie-react-hooks';
+import { Link } from 'react-router-dom';
 import LawFirmItem from './LawFirmItem';
+import styles from './LawFirmsList.module.css';
 
 const LawFirmsList = () => {
   const lawFirmList = useLiveQuery(() => db.lawFirms.toArray());
 
   return (
     <div>
+      {lawFirmList?.length === 0 && (
+        <div className={styles.notFound}>
+          <Link to="/addLawFirm">
+            Niestety, nic nie znaleziono.. Dodaj swoją pierwszą kancelarię!
+          </Link>
+        </div>
+      )}
       {lawFirmList?.map(({ name, description, location }) => (
         <LawFirmItem
           name={name}
